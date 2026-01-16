85409f9852c907ed2018612961bfe2c8f856afa3
Allwyn46 <allwyn153@gmail.com>
2026-01-16 18:42:29 +0530

    Added few questions and implemented the initial quiz questions

--- quiz.py
+++ quiz.py
@@ -6,3 +6,23 @@ if playing != "yes":
     quit()
 else:
     print("Finally a worthy oponent")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("correct!")
+else:
+    print("Better Luck next time")
+
+
+answer1 = input("What is the brain of the computer called? ")
+if answer1 == "cpu":
+    print("correct!")
+else:
+    print("Better Luck next time")
+
+
+answer2 = input("What type of memory is temporary and loses data when powered off? ")
+if answer2 == "ram":
+    print("correct!")
+else:
+    print("Better Luck next time")
