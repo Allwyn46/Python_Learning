76a161f2b95dc024dd0aaa8a2cbbf538b89e02e3
Allwyn46 <allwyn153@gmail.com>
2026-02-20 18:22:37 +0530

    Added score

--- quiz.py
+++ quiz.py
@@ -1,6 +1,7 @@
 print("Welcome to my Quiz Game!")
 
 playing = input("Do you wanna play? ")
+score = 0
 
 if playing.lower() != "yes":
     quit()
@@ -10,6 +11,7 @@ else:
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("correct!")
+    score += 1
 else:
     print("Better Luck next time")
     quit()
@@ -18,6 +20,7 @@ else:
 answer1 = input("What is the brain of the computer called? ")
 if answer1.lower() == "cpu":
     print("correct!")
+    score += 1
 else:
     print("Better Luck next time")
     quit()
@@ -26,6 +29,7 @@ else:
 answer2 = input("What type of memory is temporary and loses data when powered off? ")
 if answer2.lower() == "ram":
     print("correct!")
+    score += 1
 else:
     print("Better Luck next time")
     quit()
