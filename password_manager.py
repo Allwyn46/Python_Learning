55c7f6a454590a76e23e4b4e0b97dd06380ad29d
Allwyn46 <allwyn153@gmail.com>
2026-03-04 17:41:22 +0530

    Added password manager

---
+++ password_manager.py
@@ -0,0 +1,24 @@
+master_pwd = input("Enter the master password: ")
+
+def view():
+    pass
+
+def add():
+    username = input("username: ")
+    password = input("password: ")
+
+    with open("passwords.txt", 'a') as f:
+        f.write(username + "|" + password + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view all passwords? or press q to quit (view/add/q): ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Enter a valid input")
+        continue
