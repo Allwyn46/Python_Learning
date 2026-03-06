9eb7d25b6336873f27841011ad074ede4638fef5
Allwyn46 <allwyn153@gmail.com>
2026-03-06 19:22:49 +0530

    Added functions to encrypt and decrypt passwords using fernet

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,28 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
+
+write_key() # Generates the key file 
+'''
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close
+    return key
+
+
 master_pwd = input("Enter the master password: ")
+encrypt_key = load_key() + master_pwd.bytes
+fer = Fernet(encrypt_key)
+
+
+
+
 
 def view():
     with open("passwords.txt", 'r') as f:
@@ -12,7 +36,7 @@ def add():
     password = input("password: ")
 
     with open("passwords.txt", 'a') as f:
-        f.write(username + "|" + password + "\n")
+        f.write(username + "|" + fer.encrypt(password.encode()) + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view all passwords? or press q to quit (view/add/q): ").lower()
